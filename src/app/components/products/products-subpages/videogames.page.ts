ea1c8b165b960893672f36b88a363e887ce66112
gi-ga-dev <cloud.gianluca@gmail.com>
2022-11-14 14:54:26 +0100

    deploy test

--- src/app/components/products/products-subpages/videogames.page.ts
+++ src/app/components/products/products-subpages/videogames.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from 'src/app/services/auth.service';
+import { MatModalVideogameComponent } from '../mat-modal-videogame/mat-modal-videogame.component';
 
 @Component({
   templateUrl: './videogames.page.html',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideogamesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, public dialog: MatDialog) { }
 
   ngOnInit(): void { }
+
+  openVideogameDialog() {
+    const dialogRef = this.dialog.open(MatModalVideogameComponent, { panelClass: 'create-vg-dialog-cont' });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
 }
